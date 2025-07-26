0affa55deba183ff90626a9818922be5795e42aa
mercycermy <mercycermy5@gmail.com>
2025-07-25 20:59:08 -0700

    nav bar bg color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -60,7 +60,7 @@ const Navbar = () => {
             to={item.href}
             className={`text-white px-4 py-2 rounded-full font-medium transition duration-200 ${
               location.pathname === item.href
-                ? 'bg-white text-[#f8b13d] font-bold'
+                ? 'bg-accent text-[#f8b13d] font-bold'
                 : 'hover:bg-white/30'
             }`}
           >
